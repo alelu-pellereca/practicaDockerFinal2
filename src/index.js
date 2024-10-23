db08d1a0a832bb17fea6a522a92d4777edc4c48c
alelu-pellereca <alejandrapellegrini@gmail.com>
2024-10-23 13:26:23 +0200

    wveEWE

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-const express = require('express')
-const app = express()
+const app = require('./app');
 
-app.listen(3000)
+const PORT = 3000//process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ---- ${PORT}`);
+});
