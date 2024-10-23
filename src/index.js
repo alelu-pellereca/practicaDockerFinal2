085faa0d3b07b7c0decdcaa9d1fc525359a7434b
alelu-pellereca <alejandrapellegrini@gmail.com>
2024-10-23 13:36:01 +0200

    egWGW

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const app = require('./app');
+const express = require('express')
+const app = express()
 
-const PORT = 3000//process.env.PORT || 3000;
+app.listen(3000)
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ---- ${PORT}`);
-});
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
